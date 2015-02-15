1ed3fcc3dab96712b9b9163f11b789bdf87686b5
thakyoko@gmail.com <thakyoko@gmail.com>
2015-02-15 22:33:21 +0700

    Initial Solution
    Initial Solution ASP.NET MVC with Semantic-UI
    [packages]
    Antlr V.3.5.0.2
    bootstrap V.3.3.2
    EntityFramework V.6.1.2
    jQuery V.2.1.3
    jQuery.Validation V.1.13.1
    Microsoft.AspNet.Identity.Core V.2.1.0
    Microsoft.AspNet.Identity.EntityFramework V.2.1.0
    Microsoft.AspNet.Identity.Owin V.2.1.0
    Microsoft.AspNet.Mvc V.5.2.3
    Microsoft.AspNet.Razor V.3.2.3
    Microsoft.AspNet.Web.Optimization V.1.1.3
    Microsoft.AspNet.WebPages V.3.2.3
    Microsoft.jQuery.Unobtrusive.Validation V.3.2.3
    Microsoft.Owin V.3.0.0
    Microsoft.Owin.Host.SystemWeb V.3.0.0
    Microsoft.Owin.Security V.3.0.0
    Microsoft.Owin.Security.Cookies V.3.0.0
    Microsoft.Owin.Security.Facebook V.3.0.0
    Microsoft.Owin.Security.Google V.3.0.0
    Microsoft.Owin.Security.MicrosoftAccount V.3.0.0
    Microsoft.Owin.Security.OAuth V.3.0.0
    Microsoft.Owin.Security.Twitter V.3.0.0
    Microsoft.Web.Infrastructure V.1.0.0.0
    Modernizr V.2.8.3
    Newtonsoft.Json V.6.0.8
    Owin V.1.0
    Respond V.1.4.2
    WebGrease V.1.6.0

---
+++ WebUI/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            db.Database.Initialize(true);
+
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            db.Database.Delete();
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
